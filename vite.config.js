d0d9d7a9d80925042ac319c31a57af267f380221
moiznisum <msiddique@nisum.com>
2024-11-12 18:28:27 +0500

    edit and delete jobs

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vue-jobs-backend.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') 
       }
